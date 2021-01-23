6ef0a176cc95a18bddebf8e5ad8195ec9a101e66
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-01-23 18:40:18 +0100

    Quiz-app  adding array of objects with questions and answers

---
+++ Quiz-app/js/script.js
@@ -0,0 +1,46 @@
+const quizData = [
+{
+    question: 'how much years have Donald Trump?',
+    a: '101',
+    b: '75',
+    c: '67',
+    d: '84',
+    correct:'b'
+},
+{
+    question: 'What is the the most used programming lenguage in 2020?',
+    a: 'Java',
+    b: 'C',
+    c: 'Paython',
+    d: 'JavaScript',
+    correct:'d'
+},
+
+{
+    question: 'Who is the President of US?',
+    a: 'Obi Wan Kenobi',
+    b: 'Donald Trump',
+    c: 'Joe Biden',
+    d: 'Barack Obama',
+    correct:'c'
+},
+
+{
+    question: 'What does HTML stand for?',
+    a: 'Hypertext Markup Langueage',
+    b: 'Cascadion Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Helicopters Terminals Motorboats Lamborguines',
+    correct:'a'
+},
+
+{
+    question: 'What Year was JavaScript Launched?',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'None of the above',
+    correct:'d'
+}
+
+]
