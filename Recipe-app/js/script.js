e886eafc5e0b9eb2cc20a43a2617ae17adae2ec2
Aleksandar Milenkovic <aleksandar.93m@gmail.com>
2021-01-25 21:13:29 +0100

    Recipe-app random meal generation Fun.

--- Recipe-app/js/script.js
+++ Recipe-app/js/script.js
@@ -1,9 +1,15 @@
+const meals = document.getElementById('meals');
+
+
 getRandomMeal();
 
 async function getRandomMeal() {
-    const randomMeal = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-   console.log(randomMeal);
+    const resp = await (await fetch("https://www.themealdb.com/api/json/v1/1/random.php"));
 
+   const respData = await resp.json();
+   const randomMeal = respData.meals[0];
+
+    addMeal(randomMeal, true);
 }
 
 async function getMealById(id){
@@ -13,4 +19,27 @@ async function getMealById(id){
 async function getMealsBySearch(name){
     const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata" + name)
 }
+
+function addMeal(mealData, random = false){
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+    meal.innerHTML = `
+        <div class="meal-header">
+                ${random ? `
+                <span class="random">
+                    Random Recipe
+                </span>` : ''}
+                <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+            </div>
+            <div class="meal-body">
+                <h4>${mealData.strMeal}</h4>
+                <button class="fav-btn">
+                    <i class="fas fa-heart"></i>
+                </button>
+            </div>
+        
+    `;
+    meals.appendChild(meal);
+}
+
 
